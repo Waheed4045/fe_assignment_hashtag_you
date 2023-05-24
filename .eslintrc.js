79ff53ae20dbfaf47ba6d966614e17fce6759ef8
WaheedAhmad <hamza.saleemi@sio.com>
2023-05-25 00:40:39 +0200

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "overrides": [],
+  "parser": "@babel/eslint-parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "rules": {}
+}
---
+++ src/app/components/Meals/AvailableMeals.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+
+import classes from "./AvailableMeals.module.css";
+
+const AvailableMeals = (props) => {
+	const [products, setProducts] = useState([]);
+	const [page, setPage] = useState(1);
+
+	useEffect(async () => {
+		const response = await fetch(
+			`http://127.0.0.1:5000/products?_page=${page}&_limit=6`
+		);
+		const data = await response.json();
+		setProducts(data);
+	}, []);
+
+	const loadMoreProducts = async () => {
+		const response = await fetch(
+			`http://127.0.0.1:5000/products?_page=${page + 1}&_limit=6`
+		);
+		const data = await response.json();
+		setProducts((prevData) => {
+			return [...prevData, ...data];
+		});
+		setPage(page + 1);
+	};
+	const mealsList = products.map((product) => {
+		return (
+			<MealItem
+				id={product.id}
+				key={product.id}
+				name={product.title}
+				description={product.description}
+				price={product.price}
+				image={product?.images[0]}
+			/>
+		);
+	});
+
+	return (
+		<section className={classes.meals}>
+			<Card>
+				<ul>{mealsList}</ul>
+				<button onClick={loadMoreProducts}>Load More</button>
+			</Card>
+		</section>
+	);
+};
+
+export default AvailableMeals;
